4c1ce962cb6e3f14e2cf1465abbcb87321cc2eae
David Sherret <dsherret@gmail.com>
2021-06-13 17:15:47 -0400

    chore: Fix npm release.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # dprint - JS Wasm Formatter
 
 JS formatter for dprint Wasm plugins.
+
+## Deno
+
+```ts
+import {
+  createStreaming,
+  GlobalConfiguration,
+} from "https://deno.land/x/dprint@x.x.x/mod.ts";
+
+const globalConfig: GlobalConfiguration = {
+  indentWidth: 2,
+  lineWidth: 80,
+};
+const tsFormatter = await createStreaming(
+  // check https://plugins.dprint.dev/ for latest plugin versions
+  fetch("https://plugins.dprint.dev/typescript-0.46.0.wasm"),
+);
+
+tsFormatter.setConfig(globalConfig, {
+  semiColons: "asi",
+});
+
+// outputs: "const t = 5\n"
+console.log(tsFormatter.formatText("file.ts", "const   t    = 5;"));
+```
+
+## Node.js
+
+```ts
+import { createFromBuffer } from "@dprint/wasm-formatter";
+
+const formatter = createFromBuffer(fs.readFileSync("./json.wasm"));
+
+console.log(formatter.formatText("test.json", "{test: 5}"));
+```
--- deployment/npm/setup.js
+++ deployment/npm/setup.js
@@ -1,6 +1,10 @@
 // @ts-check
 import * as path from "https://deno.land/std@0.98.0/path/mod.ts";
-import { Project, ScriptTarget } from "https://deno.land/x/ts_morph@11.0.1/mod.ts";
+import {
+  ModuleKind,
+  Project,
+  ScriptTarget,
+} from "https://deno.land/x/ts_morph@11.0.1/mod.ts";
 
 const version = Deno.args[0];
 
@@ -17,21 +21,24 @@ Deno.writeTextFileSync(
 
 // emit mod.ts as index.js in this folder
 const project = new Project({
-    compilerOptions: {
-        target: ScriptTarget.ES2015,
-        declaration: true,
-        outDir: dirname(),
-    },
+  compilerOptions: {
+    target: ScriptTarget.ES2015,
+    module: ModuleKind.CommonJS,
+    declaration: true,
+    outDir: dirname(),
+  },
 });
-const sourceFile = project.addSourceFileAtPath(path.join(dirname(), "../../mod.ts"));
+const sourceFile = project.addSourceFileAtPath(
+  path.join(dirname(), "../../mod.ts"),
+);
 const diagnostics = project.getPreEmitDiagnostics();
 if (diagnostics.length > 0) {
-    console.error(project.formatDiagnosticsWithColorAndContext(diagnostics));
-    throw new Error("Had diagnostics.");
+  console.error(project.formatDiagnosticsWithColorAndContext(diagnostics));
+  throw new Error("Had diagnostics.");
 }
 await sourceFile.emit();
 
 function dirname() {
-    const result = new URL(".", import.meta.url).pathname;
-    return Deno.build.os === "windows" ? result.substring(1) : result;
+  const result = new URL(".", import.meta.url).pathname;
+  return Deno.build.os === "windows" ? result.substring(1) : result;
 }
